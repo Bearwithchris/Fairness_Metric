bced7a8127582a0b104cc47e0b9e5df95a289b61
Bearwithchris <Christopher_teo@mymail.sutd.edu.sg>
2021-04-26 20:09:06 +0800

    Extreme Points Reran

--- scripts/Full_Run_ExtremePoints.bat
+++ scripts/Full_Run_ExtremePoints.bat
@@ -6,7 +6,7 @@ python train_attribute_clf.py celeba ./results/multi_clf --even=True --multi=Tru
 cd ../CelebA_data_split
 python .\gen_celebA_dataset.py --multi=True --mode_normal=0 --split_type test --multi=True  --multi_class_idx 20
 cd ../Testing_metrics
-for /l %%x in (0, 1, 1) do python sample_test.py --multi=True --multi_class_idx 20 --index=%%x
+for /l %%x in (0, 1, 2) do python sample_test.py --multi=True --multi_class_idx 20 --index=%%x
 
 cd ../Data_prep
 python .\preprocess_celeba_multi.py --split_type=train --multi_class_idx 20 8
